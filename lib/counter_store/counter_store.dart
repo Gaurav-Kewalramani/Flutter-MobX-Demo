2823e70c994a48f7df0e705b95072cc72c8a5afe
gaurav kewalramani <gaurav.kewalramani@gmail.com>
2023-05-12 14:41:57 +0530

    final commit

---
+++ lib/counter_store/counter_store.dart
@@ -0,0 +1,15 @@
+import 'package:mobx/mobx.dart';
+
+part 'counter_store.g.dart';
+
+class CounterStore = _CounterStore with _$CounterStore;
+
+abstract class _CounterStore with Store {
+  @observable
+  var counter = 0;
+
+  @action
+  void increment() {
+    counter++;
+  }
+}
